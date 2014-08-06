83dc23ce28e74d3e06024caeabeb6f91cef72933
Glyn Normington <gnormington@pivotal.io>
2014-08-06 16:59:14 +0100

    Bring temp libcontainer API closer to reality

--- temp_libcontainer_api/container.go
+++ temp_libcontainer_api/container.go
@@ -1,7 +1,68 @@
 package temp_libcontainer_api
 
+import (
+	"github.com/docker/libcontainer"
+)
+
 type Id string
 
 type Container interface {
 	Id() Id
+
+	// Returns the current run state of the container.
+	//
+	// Errors: container no longer exists,
+	//         system error.
+	RunState() (*libcontainer.RunState, error)
+
+	// Returns the current config of the container.
+	Config() *libcontainer.Config
+
+	// Start a process inside the container. Returns the PID of the new process (in the caller process's namespace) and a channel that will return the exit status of the process whenever it dies.
+	//
+	// Errors: container no longer exists,
+	//         config is invalid,
+	//         container is paused,
+	//         system error.
+	Start(*libcontainer.ProcessConfig) (pid int, exitChan chan int, err error)
+
+	// Destroys the container after killing all running processes.
+	//
+	// Any event registrations are removed before the container is destroyed.
+	// No error is returned if the container is already destroyed.
+	//
+	// Errors: system error.
+	Destroy() error
+
+	// Returns the PIDs inside this container. The PIDs are in the namespace of the calling process.
+	//
+	// Errors: container no longer exists,
+	//         system error.
+	//
+	// Some of the returned PIDs may no longer refer to processes in the Container, unless
+	// the Container state is PAUSED in which case every PID in the slice is valid.
+	Processes() ([]int, error)
+
+	// Returns statistics for the container.
+	//
+	// Errors: container no longer exists,
+	//         system error.
+	Stats() (*libcontainer.ContainerStats, error)
+
+	// If the Container state is RUNNING or PAUSING, sets the Container state to PAUSING and pauses
+	// the execution of any user processes. Asynchronously, when the container finished being paused the
+	// state is changed to PAUSED.
+	// If the Container state is PAUSED, do nothing.
+	//
+	// Errors: container no longer exists,
+	//         system error.
+	Pause() error
+
+	// If the Container state is PAUSED, resumes the execution of any user processes in the
+	// Container before setting the Container state to RUNNING.
+	// If the Container state is RUNNING, do nothing.
+	//
+	// Errors: container no longer exists,
+	//         system error.
+	Resume() error
 }
--- temp_libcontainer_api/factory.go
+++ temp_libcontainer_api/factory.go
@@ -7,17 +7,16 @@ import (
 type Name string
 
 type Factory interface {
-// Creates a new container in the given path. A unique ID is generated for the container and
-// starts the initial process inside the container.
-//
-// Returns the new container with a running process.
-//
-// Errors:
-// Path already exists
-// Config or initialConfig is invalid
-// System error
-//
-// On error, any partially created container parts are cleaned up (the operation is atomic).
-Create(config *libcontainer.Config) (Container, error)
+	// Creates a new container. Generates a unique ID for the container and
+	// starts the initial process inside the container.
+	//
+	// Returns the new container with a running process.
+	//
+	// Errors:
+	// Config is invalid
+	// System error
+	//
+	// On error, any partially created container parts are cleaned up (the operation is atomic).
+	Create(config *libcontainer.Config) (Container, error)
 
 }
